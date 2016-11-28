78f0071548866ab27334ce4dd4128cc3883a3715
Arnis Jaundzeikars <sangsatori@users.noreply.github.com>
2016-11-28 13:11:22 +0000

    Improve comments

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
-// Uses ES6
+// Uses ES6; transpile or use latest Chrome/Firefox
 // define color schema here (hex codes from http://clrs.cc/)
+
 const BASES = [ // grayscale dark to light
   '#111', // near-black
   '#333', // dark gray
@@ -11,82 +12,95 @@ const ACCENTS = [ // bright accents
   '#FFDC00', // yellow
   '#FF4136 ' // red
 ];
+// current colour state
 let colour = {
   accent: ACCENTS[0],
   base: BASES[1]
 }
 
-// GLOBALS
+// GLOBALS, tweak as necessary
 const ENTITY_COUNT = 32;
 const LIMIT = 64; // scaling resolution
 const INNER_AREA = .8; // percentage of area for entity placement
+// interactive key map
+const KEYS = {
+  'space': 32 // toggle colour
+};
 
 // dynamic object collections
 let entities;
 let gravity;
 
-// debug stuff
-//const FR = (n) => frameRate(n);
-let running;
-let fr;
+// debug functions
+let running; // pause state
+let fr; // framerate memory
+// pause the sketch for variable inspection
 const PAUSE = () => {
   if (running) {
-    frameRate(NaN);
+    frameRate(NaN); // stops drawing
   } else {
-    frameRate(fr);
+    frameRate(fr); // retur to previous framerate
   }
-  running = !running;
+  running = !running; // toggle pause state
 }
-
 
 // utility functions
 const getSize = () => new Array(2).fill(min(windowWidth, windowHeight)); // 2-value array
 const getScale = () => min(height, width) / LIMIT * INNER_AREA; // scale factor
+
 // construct new entity objects
 const Entity = (pos, variance) => ({
-  pos, // ES6 object assignment
+  pos, // direct ES6 object assignment
   size: random(variance[0], variance[1]),
   vel: createVector(random(-.75, .75), random(-.75, .75)), // TODO proper values
   trail: new FixedArray(32)
 });
-const KEYS = {
-  'space': 32
-};
 
 // TODO create a new type of FixedArray
 class FixedArray extends Array {
   constructor(size) {
     super();
-    this._size = size;
+    // fixed size limit
+    this._size = size; // naming by convention for private attributes
   }
   push(x) {
     super.push(x);
+    // ensure length doesn't exceed size limit
     if (this.length > this._size) {
       super.shift();
     }
   }
 }
 
+// CALLBACKS
 function setup() {
+  // init global gravity well
   gravity = createVector(LIMIT, LIMIT).div(2);
+  // populate with entities
   entities = new Array(ENTITY_COUNT)
-  .fill(undefined)
-  .map(_ => Entity(
+  .fill(undefined) // so .map() works
+  .map(_ => Entity( // underscore for unneeded value
     createVector(random(LIMIT-1), random(LIMIT-1)),
     [.25, 3.25]
   ));
   createCanvas(...getSize()); // spread from single call
+  // visual adjustments
   strokeWeight(.25);
+  // all systems go
   running = true;
 }
 
 function draw() {
-  fr = frameRate();
+  fr = frameRate(); // register framerate
+
+  // update state
   var prev;
-  entities.forEach(e => {
-    prev = e.pos.copy();
+  entities.forEach(e => { // iterate over entities
+    prev = e.pos.copy(); // lock previous entity position
+    // calculate position with forces applied
     e.pos.add(e.vel.add(gravity.copy().sub(e.pos).normalize().div(LIMIT)));
-    e.trail.push([prev, e.pos.copy()]); // avoid mutation?
+    // update the traiil
+    e.trail.push([prev, e.pos.copy()]); // avoid mutation of position
   });
 
   // draw the state
@@ -95,26 +109,29 @@ function draw() {
   let offset = (min(height, width) * (1 - INNER_AREA) / 2); // allocate margins
   translate(offset, offset); // center in the middle
   scale(getScale()); // ensure correct scale
-
   // set drawing mode
   fill(colour.accent);
   stroke(colour.accent);
 
-  // draw
-  entities.forEach(e => {
+  // draw state
+  entities.forEach(e => { // parse entities with the new state
     let prev = e.pos;
-    e.trail.forEach(pair => {
+    e.trail.forEach(pair => { // lines with start & end positions
      line(pair[0].x, pair[0].y, pair[1].x, pair[1].y);
     })
+    // draw the entity
     ellipse(e.pos.x, e.pos.y, e.size);
   });
 }
 
+// INTERACTIVITY
 function mouseReleased() {
-  gravity = createVector(mouseX, mouseY).div(getScale());
+  gravity = createVector(mouseX, mouseY).div(getScale()); // account for scale
 }
+
 function keyPressed() {
   switch (keyCode) {
+    // change to random predefined color
     case KEYS['space']:
       colour.accent = random(ACCENTS);
       break;
@@ -123,7 +140,7 @@ function keyPressed() {
   }
 }
 
-// screen resize handdler
+// screen resize handler
 function windowResized() {
   resizeCanvas(...getSize()); // spread from single call
 }
